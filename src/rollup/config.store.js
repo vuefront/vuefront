b1468774ea91681072b4a7dd9849173ff38704f6
dmitriyzhuk <dmitriyzhuk@gmail.com>
2019-04-11 14:48:05 +0300

    fix rollup on mac

--- src/rollup/config.store.js
+++ src/rollup/config.store.js
@@ -9,38 +9,38 @@ const stores = path.resolve(base, 'src/store')
 const dist = path.resolve(base, 'lib')
 const src = path.resolve(base, 'src')
 const _default = filePath => {
-  const componentPath = path.resolve(filePath).replace(stores + '\\', '')
+    const componentPath = path.resolve(filePath).replace(stores + '/', '')
 
-  const externals = ['vue', name, ...Object.keys(dependencies)]
+    const externals = ['vue', name, ...Object.keys(dependencies)]
 
-  return {
-    input: 'src/store/' + componentPath,
-    output: {
-      file: path.resolve(dist, 'store/' + componentPath)
-    },
-    external: path => {
-      let result = false
+    return {
+        input: 'src/store/' + componentPath,
+        output: {
+            file: path.resolve(dist, 'store/' + componentPath)
+        },
+        external: path => {
+            let result = false
 
-      for (const key in externals) {
-        if (path == externals[key]) {
-          result = true
-        } else if (path.indexOf(externals[key] + '/') === 0) {
-          result = true
-        }
-      }
+            for (const key in externals) {
+                if (path == externals[key]) {
+                    result = true
+                } else if (path.indexOf(externals[key] + '/') === 0) {
+                    result = true
+                }
+            }
 
-      return result
-    },
-    plugins: [
-      graphql(),
-      commonjs(),
-      alias({
-        resolve: ['.jsx', '.js', '.graphql'],
-        '~': src
-      }),
-      cleanup()
-    ]
-  }
+            return result
+        },
+        plugins: [
+            graphql(),
+            commonjs(),
+            alias({
+                resolve: ['.jsx', '.js', '.graphql'],
+                '~': src
+            }),
+            cleanup()
+        ]
+    }
 }
 
 export default _default
