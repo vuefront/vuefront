b069964f294b7841ff97fb1436d5ff565713aff9
Alexandr Krasnij <php4.drvn@gmail.com>
2019-05-15 17:55:03 +0300

    Moved files

--- src/components/elements/store/product/image.vue
+++ src/components/elements/store/product/image.vue
@@ -1,14 +1,23 @@
 <template>
   <section class="vf-product-thumbnails mb-4">
     <b-img-lazy :src="mainImage" :blank-src="mainImagelazy" fluid style="width: 100%;"/>
+    <b-row>
+      <b-col sm="12" md="3" v-for="(value, index) in product.images" :key="index">
+        <b-img-lazy :src="value.image" :blank-src="value.imageLazy" fluid style="width: 100%;"/>
+      </b-col>
+    </b-row>
   </section>
 </template>
 <script>
-import { BImgLazy } from "bootstrap-vue/es/components";
+import { BImgLazy, BRow, BCol } from "bootstrap-vue/es/components";
 import placeholder from "~/assets/img/placeholder.png";
+import PicZoom from 'vue-piczoom'
 export default {
   components: {
-    BImgLazy
+    PicZoom,
+    BImgLazy,
+    BRow,
+    BCol
   },
   props: ["product"],
   computed: {
