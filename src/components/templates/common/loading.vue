5dd47841a717628428fc5b29b16ce52348a9e4ef
Alexandr Krasnij <php4.drvn@gmail.com>
2019-06-07 17:44:32 +0300

    Added animation for loading

--- src/components/templates/common/loading.vue
+++ src/components/templates/common/loading.vue
@@ -4,22 +4,22 @@
       <b-container>
         <b-row class="mt-3" align-h="center">
           <b-col sm="3" class="mb-2">
-            <tb-skeleton shape="circle" :aspect-ratio="0.3" width="30%" bg-color="#eee" class="mx-auto mb-2" duration="2"></tb-skeleton>
-            <tb-skeleton shape="radius" width="100%" :aspect-ratio=".1" bg-color="#eee"></tb-skeleton>
+            <tb-skeleton theme="opacity" shape="circle" :aspect-ratio="0.3" width="30%" bg-color="#eee" class="mx-auto mb-2" duration="2"></tb-skeleton>
+            <tb-skeleton theme="opacity" shape="radius" width="100%" :aspect-ratio=".1" bg-color="#eee"></tb-skeleton>
           </b-col>
         </b-row>
         <b-row class="mt-3">
           <b-col sm="12" class="mb-2">
-            <tb-skeleton shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
+            <tb-skeleton theme="opacity" shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
           </b-col>
           <b-col sm="12" class="mb-2">
-            <tb-skeleton shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
+            <tb-skeleton theme="opacity" shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
           </b-col>
           <b-col sm="12" class="mb-2">
-            <tb-skeleton shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
+            <tb-skeleton theme="opacity" shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
           </b-col>
           <b-col sm="12" class="mb-2">
-            <tb-skeleton shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
+            <tb-skeleton theme="opacity" shape="radius" width="100%" :aspect-ratio=".02" bg-color="#eee" duration="2"></tb-skeleton>
           </b-col>
         </b-row>
       </b-container>
