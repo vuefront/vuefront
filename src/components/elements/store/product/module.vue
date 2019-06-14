1ba1d87f556e6abddd84d4c22a8c397649ce275c
Alexandrvn <php4.drvn@gmail.com>
2019-06-14 17:21:24 +0300

    Fix bug

--- src/components/elements/store/product/module.vue
+++ src/components/elements/store/product/module.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home-page__latest_products mb-5">
+  <div class="home-page__latest_products mb-5" v-if="items.length > 0">
     <div class="home-page__latest_products_title text-sm-center mb-5 h6">
       <slot></slot>
     </div>
