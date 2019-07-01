c039a2d227237caf70db7743059f022edd319615
Alexandrvn <php4.drvn@gmail.com>
2019-07-01 17:13:26 +0300

    Fix bug

--- src/components/elements/common/contact/location.vue
+++ src/components/elements/common/contact/location.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="location-section">
+  <section class="location-section mb-3">
     <b-card class="shadow">
       <b-card-body class="px-5 py-5">
         <div style="min-height: 250px;">
           <h2 class="h2 font-weight-bold mb-4">{{location.address}}</h2>
-          <h5 class="mt-4 mb-2">T: {{location.telephone}}</h5>
+          <h5 class="mt-4 mb-2" v-if="location.telephone !== ''">T: {{location.telephone}}</h5>
           <p class="lead">{{location.comment}}</p>
         </div>
       </b-card-body>
