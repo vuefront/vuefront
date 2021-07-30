fdd18f1660b1af7fe88ce6821b5a39839d6bb6f5
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-07-30 14:02:47 +0300

    fix: fix for vue 3

---
+++ lib/components/molecules/skeleton/skeleton.vue
@@ -0,0 +1,14 @@
+<template>
+  <Skeletor v-bind="$attrs"></Skeletor>
+</template>
+<script>
+import { defineComponent } from "vue";
+import "vue-skeletor/dist/vue-skeletor.css";
+import { Skeletor } from "vue-skeletor";
+export default defineComponent({
+  components: {
+    Skeletor,
+  },
+  inheritAttrs: false,
+});
+</script>
