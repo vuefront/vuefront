932e9a53b4050f108acb54dcbba6c9f1868c7612
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-09-17 17:17:08 +0300

    fix: fix error template

--- lib/components/templates/common/error/error.vue
+++ lib/components/templates/common/error/error.vue
@@ -1,17 +1,9 @@
 <template>
-  <vf-t-common-layout class="not-found">
-    <vf-m-row align-h="center">
-      <vf-m-col lg="7" class="text-center">
-        <vf-a-heading :level="1" tag="h6" class="mb-5 font-normal">{{
-          $t("elements.common.error.textError")
-        }}</vf-a-heading>
-      </vf-m-col>
-    </vf-m-row>
-  </vf-t-common-layout>
+  <vf-m-row align-h="center">
+    <vf-m-col lg="7" class="text-center">
+      <vf-a-heading :level="1" tag="h6" class="mb-5 font-normal">{{
+        $t("elements.common.error.textError")
+      }}</vf-a-heading>
+    </vf-m-col>
+  </vf-m-row>
 </template>
-<script lang="ts" setup>
-import { useStore } from "vuex";
-
-const store = useStore();
-store.commit("common/breadcrumbs/setLoaded", true);
-</script>
