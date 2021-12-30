f1c3ff1401a4c647820d9a06aaa01b7e0340d76f
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-12-30 10:20:16 +0200

    v0.4.11

--- src/components/extensions/account/account-links/account-links.vue
+++ src/components/extensions/account/account-links/account-links.vue
@@ -4,22 +4,42 @@
       $t("modules.common.accountLinks.textTitle")
     }}</vf-a-heading>
     <ul class="list-unstyled">
-      <li>
+      <li v-if="includes(links, 'login')">
         <vf-a-link to="/account/login">{{
           $t("modules.common.accountLinks.signInText")
         }}</vf-a-link>
       </li>
-      <li>
+      <li v-if="includes(links, 'register')">
         <vf-a-link to="/account/register">{{
           $t("modules.common.accountLinks.registerText")
         }}</vf-a-link>
       </li>
-      <li>
+      <li v-if="includes(links, 'account')">
         <vf-a-link to="/account">{{
           $t("modules.common.accountLinks.myAccountText")
         }}</vf-a-link>
       </li>
-      <li>
+      <li v-if="includes(links, 'edit-account')">
+        <vf-a-link to="/account/edit">{{
+          $t("modules.common.accountLinks.editAccountText")
+        }}</vf-a-link>
+      </li>
+      <li v-if="includes(links, 'password')">
+        <vf-a-link to="/account/password">{{
+          $t("modules.common.accountLinks.passwordText")
+        }}</vf-a-link>
+      </li>
+      <li v-if="includes(links, 'address')">
+        <vf-a-link to="/account/address">{{
+          $t("modules.common.accountLinks.addressText")
+        }}</vf-a-link>
+      </li>
+      <li v-if="includes(links, 'account')">
+        <vf-a-link to="/account">{{
+          $t("modules.common.accountLinks.myAccountText")
+        }}</vf-a-link>
+      </li>
+      <li v-if="includes(links, 'wishlist')">
         <vf-a-link to="/account/wishlist">{{
           $t("modules.common.accountLinks.wishlistText")
         }}</vf-a-link>
@@ -28,5 +48,18 @@
   </section>
 </template>
 <script>
-export default {};
+import { includes } from "lodash";
+export default {
+  props: {
+    links: {
+      type: Array,
+      default() {
+        return ["login", "register", "account", "wishlist"];
+      },
+    },
+  },
+  methods: {
+    includes,
+  },
+};
 </script>
